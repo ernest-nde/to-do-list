417194769a284a9ff660a977b322d134c63ec60e
ernest-nde <ernestmekontso.codevibe@gmail.com>
2023-07-19 16:05:10 -0300

    ajustement des output des taches créées

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,7 @@
 export default function ListItem({itemData, deleteTodo}) {
   return (
     <li className="p-2 bg-zinc-200 mb-2 rounded flex">
-      <span>{itemData.content}</span>
+      <span className="w-fit overflow-scroll">{itemData.content}</span>
       <button onClick={() => deleteTodo(itemData.id)} className="ml-auto bg-red-600 w-6 h-6 rounded-full text-zinc-200">X</button>
     </li>
   )
