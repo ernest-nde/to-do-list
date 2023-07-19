ce6fbe44181efabf7dd9e675898578e5c4836dcc
ernest-nde <ernestmekontso.codevibe@gmail.com>
2023-07-19 15:52:55 -0300

    correction du bouton de suppression

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,7 +2,7 @@ export default function ListItem({itemData, deleteTodo}) {
   return (
     <li className="p-2 bg-zinc-200 mb-2 rounded flex">
       <span>{itemData.content}</span>
-      <button onClick={() => deleteTodo(itemData)} className="ml-auto bg-red-600 w-6 h-6 rounded-full text-zinc-200">X</button>
+      <button onClick={() => deleteTodo(itemData.id)} className="ml-auto bg-red-600 w-6 h-6 rounded-full text-zinc-200">X</button>
     </li>
   )
 }
