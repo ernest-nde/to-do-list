6bcf78c2d80b8fc97a4ee00b01906213064c999e
ernest-nde <ernestmekontso.codevibe@gmail.com>
2023-07-19 15:50:26 -0300

    commit to_do_list_003

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
+import ListItem from "./components/ListItem";
 
 function App() {
 
   const [todoList, setTodoList] = useState([
-    {
-      id: nanoid(10),
-      content: "Item 1"
-    }
+    // {
+    //   id: nanoid(10),
+    //   content: "item1"
+    // },
   ]);
-  console.log(todoList);
+
+  const [todo, setTodo] = useState("");
+  const [showValidation, setShowValidation] = useState(false);
+
+  function deleteTodo(id) {
+    setTodoList(todoList.filter(todo => todo.id !== id));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (todo === "") {
+      setShowValidation(true);
+      return;
+    }
+    setTodoList([...todoList, {
+      id: nanoid(10),
+      content: todo
+    }]);
+    setTodo("");
+    setShowValidation(false);
+  }
 
   return (
-    <div className="h-screen bg-slate-900">
+    <div className="h-screen">
       <div className="max-w-4xl mx-auto pt-20 px-6">
         <h1 className="text-3xl text-slate-100 mb-4">La To-do Liste</h1>
         
-        <form className="mb-10">
-          <label htmlFor="todo-item" className="text-slate-50">Ajouter une tâche à faire !</label>
+        <form onSubmit={handleSubmit} className="mb-10">
+          <label htmlFor="todo-item" className="text-slate-50 italic">Ajouter une tâche à faire !</label>
           <input
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
             type="text"
             className="mt-1 w-full rounded"
+            placeholder="Entrez une nouvelle tâche"
           />
+          {showValidation && (<p className="text-xs text-red-500 italic ease-in">Bien vouloir renseigner une tâche</p>)}
           <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
         </form>
         <ul>
-          
+          {todoList.length === 0 && (<li className="text-slate-50 text-md italic text-yellow-500 ease-in-out animate-pulse">Pas d&apos;items à afficher!...</li>)}
+          { todoList.length > 0 && todoList.map((item) => ( <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo}/> )) }
         </ul>
       </div>
     </div>
